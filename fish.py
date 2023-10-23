eb02da1bef48dad33c636268c1ffba9ef558d0ce
mrob1213 <m265382@usna.edu>
2023-10-23 15:23:43 -0400

    123

--- fish.py
+++ fish.py
@@ -1,5 +1,8 @@
 import pygame
+import random
 
+MIN_SPEED = 0.5
+MAX_SPEED = 3.0
 class Fish(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
@@ -9,7 +12,12 @@ class Fish(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.x = x
         self.y = y
+        self.speed = random.uniform(MIN_SPEED, MAX_SPEED)
         self.rect.center = (x,y)
+
+    def update(self):
+        self.x -= self.speed
+        self.rect.x = self.x
 
     def draw(self, surf):
         surf.blit(self.image, self.rect)
