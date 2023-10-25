6bfad4acb56bbbfde85b0cf23e8ae7ae68b3e398
mrob1213 <m265382@usna.edu>
2023-10-25 15:21:17 -0400

    arrows

--- game4/game4.py
+++ game4/game4.py
@@ -15,9 +15,9 @@ pygame.display.set_caption('beachside1')
 
 clock = pygame.time.Clock()
 def draw_background(surf):
-    water = pygame.image.load('assets/sprites/water.png').convert()
-    sand = pygame.image.load('assets/sprites/sand_top.png').convert()
-    seagrass = pygame.image.load('assets/sprites/seagrass.png').convert()
+    water = pygame.image.load('../assets/sprites/water.png').convert()
+    sand = pygame.image.load('../assets/sprites/sand_top.png').convert()
+    seagrass = pygame.image.load('../assets/sprites/seagrass.png').convert()
 
     sand.set_colorkey((0,0,0))
     seagrass.set_colorkey((0,0,0))
@@ -32,7 +32,7 @@ def draw_background(surf):
     for _ in range(7):
         x = random.randint(0, screen_width)
         surf.blit(seagrass, (x,screen_height-tile_size*2))
-    custom_font = pygame.font.Font('assets/fonts/VOLACROME.ttf', 50)
+    custom_font = pygame.font.Font('../assets/fonts/VOLACROME.ttf', 50)
     text = custom_font.render('Chomp', True, (255,0,0))
     surf.blit(text, (screen_width/2-text.get_width()/2,screen_height/40))
 
